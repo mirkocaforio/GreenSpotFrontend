fd4887bdd133b1d5e97513ac19df9919b23cf0be
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-06-08 11:35:56 +0200

    Added 404 page not found

---
+++ src/routes/ErrorRoutes.jsx
@@ -0,0 +1,12 @@
+
+// project imports
+import ErrorPage from "../views/error/ErrorPage";
+
+// ==============================|| AUTHENTICATION ROUTING ||============================== //
+
+const ErrorRoutes = {
+    path: '*',
+    element: <ErrorPage />,
+};
+
+export default ErrorRoutes;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,9 +3,10 @@ import { createBrowserRouter } from 'react-router-dom';
 // routes
 import MainRoutes from './MainRoutes';
 import LoginRoutes from './AuthenticationRoutes';
+import ErrorRoutes from "./ErrorRoutes";
 
 // ==============================|| ROUTING RENDER ||============================== //
-const router = createBrowserRouter([MainRoutes, LoginRoutes], {
+const router = createBrowserRouter([MainRoutes, LoginRoutes, ErrorRoutes], {
   basename: import.meta.env.VITE_APP_BASE_NAME
 });
 
