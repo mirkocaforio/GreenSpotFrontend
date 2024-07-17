e50ad0bb239a01fec4c96666d576d66a749d59df
mirkocaforio <mirkocaforio2002@gmail.com>
2024-07-17 18:43:27 +0200

    Fixed docker compose with npm run start and build phase. Added server script

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/apiGateway-js-sdk', express.static(path.join(__dirname, 'apiGateway-js-sdk')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
