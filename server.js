7885a050d9710ccd0d475ade3817cff114f508cf
mirkocaforio <mirkocaforio2002@gmail.com>
2024-07-21 20:10:56 +0200

    Fixed server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/apiGateway-js-sdk', express.static(path.join(__dirname, 'apiGateway-js-sdk')));
+app.use('/apiGateway-js-sdk', express.static('apiGateway-js-sdk'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
